31c010dc07efbd1dad12fe8a0339249ecb697752
Persovt <mza200339@gmail.com>
2020-11-08 11:00:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const getData = async(sity) => {
+    await fetch('api.openweathermap.org/data/2.5/weather?id=2172797&appid=86181034ba903cb3066f9d04d1a27943')
+      .then((resp)=>  resp.json())
+      .then((data) => {console.log(data)})
+  }
+
+  getData()
+
   return (
-    
+    <div className="">
+      
+    </div>
   );
 }
 
