82c6578ab5e7848acc5eed48908b70b0c67ba03a
Persovt <46276626+Persovt@users.noreply.github.com>
2020-11-09 22:50:36 +0300

    Update getWeather.tsx

--- src/getWeather.tsx
+++ src/getWeather.tsx
@@ -1,7 +1,7 @@
 
 
 export const getWeather = (lat: number, lon: number) => {
-    return fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&,NC&key=11ad7657a3f04acdae239ef3d91e2974&days=7`)              
+    return fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&,NC&key=405df99ecb8d4295b278fcf9a5c0393f&days=7`)              
       .then(response => response.json())         
       .catch((error) => console.log('Error:  ' + error))         
 }
